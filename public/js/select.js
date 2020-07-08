50dd6235c8d00521c473c5a674399c5bee3fa6b8
koushikraghavan <koushik@proisc.com>
2020-07-08 16:43:14 +0530

    added select.js file

---
+++ public/js/select.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){  
+  $.noConflict();
+ 
+ $('.dynamic').change(function(){
+  if($(this).val() != '')
+  {
+   var select = $(this).attr("id");
+   var value = $(this).val();
+   var dependent = $(this).data('dependent');
+   var _token = $('input[name="_token"]').val();
+   $.ajax({
+    url:"./addproduct/fetch",
+    method:"POST",
+    data:{select:select, value:value, _token:_token, dependent:dependent},
+    success:function(result)
+    {
+     $('#subcatid').html(result);
+    }
+
+   })
+  }
+ });
+ });
