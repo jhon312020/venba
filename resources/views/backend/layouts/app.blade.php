0665d969f310b43a680daf4f9668bf60d3bef78f
koushikraghavan <koushik@proisc.com>
2020-08-02 00:17:59 +0530

    changes made

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -1,6 +1,7 @@
 @extends('adminlte::page')
 @section('css')
   <link rel="stylesheet" href="/css/admin.css">
+  
 @endsection
 @section('js')
   <script src="{{ URL::asset('js/backend.js') }}"></script>
