75f2929c2c43dfe3bbbfe85773a2dc9d6eafa2d6
koushikraghavan <koushik@proisc.com>
2020-08-05 01:12:53 +0530

    changes made

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use App\Models\Category as Category;
+use App\Models\Product as Product;
+use App\Models\Productimage as Productimage;
+use App\Models\Brand as Brand;
+use App\Models\Type as Type;
+use App\Models\Compatibility as Compatibility;
 
 class HomeController extends Controller
 {
@@ -31,7 +37,20 @@ class HomeController extends Controller
         if($role == 'admin'){
           return view('home');
         } else {
-            return view('frontend.index');
+            $productlist =  Product::select('name', 'accessories_required', 'price')
+    ->where('cat_id', 1)
+    ->get();
+     $brandlist = Brand::select('id', 'name')
+    ->get();
+    $categories = Category::select('id', 'name')
+    ->where('cat_id', null)
+    ->get();
+     $typelist = Type::select('id', 'name')
+    ->get();
+     $compatibilitylist = Compatibility::select('id', 'name')
+    ->get();
+
+           return view('frontend.index', compact('productlist','brandlist', 'typelist', 'compatibilitylist','categories'));
         }
     }
     public function profile()
