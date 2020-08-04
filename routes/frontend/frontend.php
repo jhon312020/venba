02a03dad340d60e857ac9d1e52c058e20fe10bef
koushikraghavan <koushik@proisc.com>
2020-08-05 01:15:28 +0530

    changes made

--- routes/frontend/frontend.php
+++ routes/frontend/frontend.php
@@ -2,6 +2,7 @@
 use App\Http\Controllers\Frontend\ProductlistingController;
 use App\Http\Controllers\Frontend\FrontendController;
 Route::get('/', [FrontendController::class, 'index'])->name('frontend.index');
+Route::get('/home', [FrontendController::class, 'index'])->name('frontend.index');
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout')->name('frontend.logout');
 Route::get('products/{category}', [ProductlistingController::class, 'index'])->name('product.index');
 Route::group(['prefix' => 'products/{category}'], function () {
